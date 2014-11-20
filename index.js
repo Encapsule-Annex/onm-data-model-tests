923c04896493770a70bc12f8f7aea2117d92185e
Chris Russell <chrisrus@encapsule.org>
2014-11-19 19:44:54 -0800

    Make it possible for onm to re-use this module... ... requires a change to the outer module.exports.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // index.js
+// 
+// var myDataModelDeclaration = { jsonTag: 'whatever' };
+// var validateDateModelDeclaration = require('onm-data-model-validator')().validateDataModelDeclaration;
+// validateDataModelDeclaration(myDataModelDeclaration);
+//
+// Please do not set the onm_ parameter which is reserved for use by the onm module.
 //
 
-module.exports = {
-    validateDataModelDeclaration: require('./test/test-shared-onmd-generic-suite')()
+module.exports = function (onm_) {
+
+    return {
+        validateDataModelDeclaration: require('./test/test-shared-onmd-generic-suite')(onm_)
+    };
+
 };
+
 
--- test/test-onm-data-model-validator.js
+++ test/test-onm-data-model-validator.js
@@ -1,11 +1,85 @@
 // test-onm-data-model-validator.js
 //
 
-var onm = require('onm');
-var addressBookDataModelDeclaration = require('onm/test/fixture/address-book-data-model').modelDeclaration;
-var validatorExports = require('../index');
+var packageMeta = require('../package.json');
+var assert = require('chai').assert;
 
-validatorExports.validateDataModelDeclaration(addressBookDataModelDeclaration);
+describe(packageMeta.name + " v" + packageMeta.version + " test suite.", function() {
 
+    describe("Load dependency and module-under-test.", function() {
 
+        var addressBookDataModelDeclaration = null;
+        var initializeDataModelDeclaration = function() {
+            addressBookDataModelDeclaration =  require('onm/test/fixture/address-book-data-model').modelDeclaration;
+        };
 
+        var thisModuleExports = null;
+        var initializeThisModuleExports = function() {
+            thisModuleExports = require('../index');
+        };
+
+        before(function() {
+            assert.doesNotThrow(initializeDataModelDeclaration);
+            assert.doesNotThrow(initializeThisModuleExports);
+        });
+
+        it("The address book data model declaration should have loaded.", function() {
+            assert.isDefined(addressBookDataModelDeclaration);
+            assert.isNotNull(addressBookDataModelDeclaration);
+            assert.isObject(addressBookDataModelDeclaration);
+        });
+
+        it("This module's exports should be defined and non-null.", function() {
+            assert.isDefined(thisModuleExports);
+            assert.isNotNull(thisModuleExports);
+        });
+
+        it("This module should export a function.", function() {
+            assert.isFunction(thisModuleExports);
+        });
+
+        describe("Resolve this module's exported function.", function() {
+
+            var validatorExports = null;
+            var initializeValidatorExports = function() {
+                validatorExports = thisModuleExports();
+            };
+
+            before(function() {
+                assert.doesNotThrow(initializeValidatorExports);
+            });
+
+            it("Calling this module's exported function with no parameters should return a defined/non-null value.", function() {
+                assert.isDefined(validatorExports);
+                assert.isNotNull(validatorExports);
+            });
+
+            it("The value should be an object.", function() {
+                assert.isObject(validatorExports);
+            });
+
+            it("The object should define property 'validateDataModelDeclaration'.", function() {
+                assert.property(validatorExports, 'validateDataModelDeclaration');
+            });
+
+            it("The 'validateDataModelDeclaration' property should be a function.", function() {
+                assert.isFunction(validatorExports.validateDataModelDeclaration);
+            });
+
+            describe("Invoke the 'validateDataModelDeclaration' function to validate the address boook data model.", function() {
+
+                before(function(done_) {
+                    validatorExports.validateDataModelDeclaration(addressBookDataModelDeclaration);
+                    done_();
+                });
+
+                it("Execute the dynamic test suite.", function() {
+                    assert.isTrue(true);
+                });
+            });
+
+        });
+
+    });
+
+});
